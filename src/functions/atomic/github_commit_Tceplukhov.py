1ae127fc5b7d0d06e3f3ba800d0476d5a696b8ff
kakitka <164000904+kakitka@users.noreply.github.com>
2025-01-11 14:39:04 +0300

    Update github_commit_tceplukhov.py

--- src/functions/atomic/github_commit_Tceplukhov.py
+++ src/functions/atomic/github_commit_Tceplukhov.py
@@ -1,55 +1,50 @@
-import requests
-import time
 from typing import List
+import requests
 import telebot
 from telebot import types
 from telebot.callback_data import CallbackData
 from bot_func_abc import AtomicBotFunctionABC
 
 class AtomicSearchArtistBotFunction(AtomicBotFunctionABC):
-    commands: List[str] = ["Text_Generate", "Genre_Generate"]
+    """docstring"""
+    commands: List[str] = ["Text_Generate", "genre_generate"]
     authors: List[str] = ["IHVH"]
     about: str = "Генерация текста заданой длины!"
     description: str = """В поле  *description* поместите подробную информацию о работе функции.
     Описание способов использования, логики работы. Примеры вызова функции - /ebf 
     Возможные параметры функции `/example`  """
     state: bool = True
-
-    bot: telebot.TeleBot
     example_keyboard_factory: CallbackData
+    bot: telebot.TeleBot
 
-    
     def set_handlers(self, bot: telebot.TeleBot):
 
         self.bot = bot
 
         @bot.message_handler(commands=self.commands[0])
-        def SearchArtistInfo_handler(message: types.Message):
+        def Search_artist_info_handler(message: types.Message):
             bot.reply_to(message, "Введите количество генерируемых строк:")
-            bot.register_next_step_handler(message, Handle_user_input)
+            bot.register_next_step_handler(message, handle_user_input)
             
-        def Text_Generator(length_Text):
+        def text_generator(length_text):
             url = "https://binaryjazz.us/wp-json/genrenator/v1/story"
-            length_Text_url = f"{url}/{length_Text}/"
-            response = requests.get(length_Text_url)
-            Finished_Text = response.json()
-            return Finished_Text
+            length_text_url = f"{url}/{length_text}/"
+            response = requests.get(length_text_url)
+            finished_text = response.json()
+            return finished_text
         
-        def Handle_user_input(message:types.Message):
-            length_Text = message.text.strip().upper()
-            TextForSong = Text_Generator(length_Text)
-            bot.reply_to(message, f"Сгенерированный текст: \n{TextForSong}")
+        def handle_user_input(message:types.Message):
+            length_text = message.text.strip().upper()
+            text_for_song = text_generator(length_text)
+            bot.reply_to(message, f"Сгенерированный текст: \n{text_for_song}")
 
         @bot.message_handler(commands=self.commands[1])
-        def SearchArtistInfo_handler(message: types.Message):
-            Genre_text = Genre_Generate
-            bot.reply_to(message, f"Сгенерированный жанр: \n{Genre_text}")
+        def generate_genre(message: types.Message):
+            genre_text = genre_generate
+            bot.reply_to(message, f"Сгенерированный жанр: \n{genre_text}")
             
-        def Genre_Generate():
+        def genre_generate():
             url = "https://binaryjazz.us/wp-json/genrenator/v1/genre/"
             response = requests.get(url)
-            Finished_Text = response.json()
-            return Finished_Text
-        
-        
-        
+            finished_text = response.json()
+            return finished_text
